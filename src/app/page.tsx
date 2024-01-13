7aeebf5755c254ea0882ac118d85c81a8f770d14
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-13 22:34:09 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,14 +16,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            
           </a>
         </div>
       </div>
