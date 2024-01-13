9b45d014563e797f6965bd9252db4f72bfed40b4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-13 22:34:49 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
-          alt="Next.js Logo"
+          
         />
       </div>
 
