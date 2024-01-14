df9d6a6ee4ed5c98fedcd9614802b9b6b793f01b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-14 13:12:33 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+           
             target="_blank"
             rel="noopener noreferrer"
           >
