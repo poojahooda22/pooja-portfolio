df3ff03bb3ca7cf4272216eb752c2fdc75efe4a0
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-14 13:15:43 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,8 @@ export default function Home() {
         from-zinc-200 pb-6 pt-8 backdrop-blur-2xl 
         dark:border-neutral-800 dark:bg-zinc-800/30 
         dark:from-inherit lg:static 
-        lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 
+        lg:dark:bg-zinc-800/30">
           
           
         </p>
