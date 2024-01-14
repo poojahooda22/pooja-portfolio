e442ab7c7ff8bc3bfd78c18af3e3eeb9fd0873b0
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-14 13:12:43 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           <a
             
            
-            target="_blank"
+            
             rel="noopener noreferrer"
           >
             
