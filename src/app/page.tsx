3fa076a72aa0a26b6baa3bf81829576694e195d7
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-14 13:18:20 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,8 @@ export default function Home() {
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent 
-          px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 
+          px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 
+          hover:dark:border-neutral-700 
           hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
