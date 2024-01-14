3a2b9cbed0fbb55e2d3f25c1aaf9b9e295079394
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-14 13:13:10 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,15 +9,10 @@ export default function Home() {
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            
-           
+         
             
             
-          >
-            
-            
-          </a>
+      
         </div>
       </div>
 
