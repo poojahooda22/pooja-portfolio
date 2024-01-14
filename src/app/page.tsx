57833e6621cdbb2bad03d51cd069cd235595c945
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-14 13:18:08 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,8 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            <span className="inline-block transition-transform group-hover:translate-x-1 
+            motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
