de05b92688ca28709a344406efbe3b1061bced12
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-15 15:11:03 +0530

    updated hero

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -15,6 +15,16 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
+      <div className="grid min-h-[70vh grid-cols-1 md:grid-cols-2 items-center">
+        <div>
+          <h1
+            className="" 
+            aria-label={slice.primary.first_name}
+          >
+            
+          </h1>
+        </div>
+      </div>
       {/* <>{slice.primary.first_name}</> */}
       {/* <>{slice.primary.last_name}</> */}
       {/* <>{slice.primary.tag_line}</> */}
