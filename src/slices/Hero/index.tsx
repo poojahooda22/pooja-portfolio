8d2e52d1ff532481e28359a11ab80c26212d086c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-15 21:38:00 +0530

    updated

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -28,7 +28,8 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
               {slice.primary.last_name}
             </span>
           </h1>
-          <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl 
+          <span 
+            className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl 
             font-bold uppercase tracking-[.2em]">
             {slice.primary.tag_line}
           </span>
