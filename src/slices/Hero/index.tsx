154439451cdeacdc383a592510c9131b7766ceed
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-16 15:36:07 +0530

    updated

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -5,6 +5,7 @@ import { useEffect, useRef } from "react";
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import gsap from "gsap";
+import Bounded from "@/components/Bounded";
 
 /**
  * Props for `Hero`.
@@ -72,7 +73,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
 
 
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       ref={component}
@@ -100,7 +101,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           </span>
         </div>
       </div>
-    </section>
+    </Bounded>
   );
 };
 
