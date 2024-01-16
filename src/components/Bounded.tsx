32f9e3495e7b1166f39349e8eb4805cc9cfc3887
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-16 15:02:51 +0530

    updated

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,17 +1,20 @@
 import Recat from 'react';
 
+type BoundedProps = {
+    as?: React.ElementType;
+}
+
 const Bounded = React.forwardRef<HTMLDivElement, BoundedProps> (
     ({ as: Comp = "section", className, children, ...restProps}, ref) => {
         return (
             <Comp
                 ref={ref} 
-                className={}
+                className={"px-4 py-10 md:px-6 md:py-14 lg:py-16"}
                 {...restProps}
             >
                 <div>
                     {children}
                 </div>
-
             </Comp>
         )
     }
