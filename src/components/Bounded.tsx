646d64c5fedab9d11de2e0fdbd7ad7ed1b4bfc23
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-16 15:08:38 +0530

    updated

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -12,7 +12,7 @@ const Bounded = React.forwardRef<HTMLDivElement, BoundedProps> (
         return (
             <Comp
                 ref={ref} 
-                className={"px-4 py-10 md:px-6 md:py-14 lg:py-16"}
+                className={clsx ("px-4 py-10 md:px-6 md:py-14 lg:py-16")}
                 {...restProps}
             >
                 <div>
