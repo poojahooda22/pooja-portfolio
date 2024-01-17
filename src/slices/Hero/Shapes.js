6bb81c9a262a50c8f7e8a0fd2d38e07ccf8c45af
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-17 13:18:55 +0530

    updated shapes

--- src/slices/Hero/Shapes.js
+++ src/slices/Hero/Shapes.js
@@ -10,7 +10,7 @@ import { gsap } from 'gsap';
 export function Shapes() {
     return (
         <div className='row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0'>
-            <Canvas>
+            <Canvas className='z-0' shadows gl={{antialias: false}} dpr={[1,1.5]} camera={{position: [0,0,25], fov: 30, near:1, far:40}}>
 
             </Canvas>
         </div>
