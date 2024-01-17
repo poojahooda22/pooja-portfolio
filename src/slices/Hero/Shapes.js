859bce7e50808e75a907dd6f7d30ac84cd959963
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-17 12:35:08 +0530

    updated gsap

--- src/slices/Hero/Shapes.js
+++ src/slices/Hero/Shapes.js
@@ -5,3 +5,4 @@ import { Canvas } from '@react-three/fiber';
 import { ContactShadows, Float, Environment } from "@react-three/drei";
 
 import { Suspense, useEffect, useRef, useState } from 'react';
+import { gsap } from 'gsap';
