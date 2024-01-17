6d27da0daad2bbad25d3d02d1a85242778742b6b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-17 13:16:15 +0530

    updated

--- src/slices/Hero/Shapes.js
+++ src/slices/Hero/Shapes.js
@@ -9,7 +9,7 @@ import { gsap } from 'gsap';
 
 export function Shapes() {
     return (
-        <div className='row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2'>
+        <div className='row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0'>
             <Canvas>
 
             </Canvas>
