fb3d566e5b44e3399aeb6d25b45bff4b67f37877
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-17 20:51:38 +0530

    updated

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -7,7 +7,7 @@ import { ContactShadows, Float, Environment } from "@react-three/drei";
 import { Suspense, useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 
-export function Shapes() {
+export default function Shapes() {
     return (
         <div className='row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0'>
             <Canvas className='z-0' shadows gl={{antialias: false}} dpr={[1,1.5]} camera={{position: [0,0,25], fov: 30, near:1, far:40}}>
