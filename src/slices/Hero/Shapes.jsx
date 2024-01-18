d92d8e52b0e9b97db766c020d1897f612d9c5bf8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-18 21:10:14 +0530

    updated

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -42,7 +42,7 @@ function Geometries() {
           {
             position: [1, -0.75, 4],
             r: 0.4,
-            geometry: new THREE.CapsuleGeometry(0.5, 1.6, 2, 16), // Pill
+            geometry: new THREE.CapsuleGeometry(0.5, 1.6, 2, 16), 
           },
           {
             position: [-1.4, 2, -4],
