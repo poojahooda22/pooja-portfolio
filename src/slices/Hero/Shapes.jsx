077a98360c463472aa97fb0a5449e7d498d38d5b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-18 21:13:23 +0530

    updated

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -52,7 +52,7 @@ function Geometries() {
           {
             position: [-0.8, -0.75, 5],
             r: 0.5,
-            geometry: new THREE.TorusGeometry(0.6, 0.25, 16, 32), 
+            geometry: new THREE.TorusGeometry(0.6, 0.25, 16, 32), // Donut
           },
           {
             position: [1.6, 1.6, -4],
