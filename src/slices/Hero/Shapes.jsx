b121336c9b1942e2fd1934ab27c56005d5ec5c46
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-19 20:57:14 +0530

    updated

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -62,7 +62,7 @@ function Geometries() {
           {
             // position: [2,1, 1.9, -1],
             // r: 0.8,
-            geometry: new THREE.TorusKnotGeometry( 0.7, 0.3, 92, 13 )
+            // geometry: new THREE.TorusKnotGeometry( 0.7, 0.3, 92, 13 )
           }
     ];
 
