fb5be76289e0ce5e0de364f156252353d4f3b7d6
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-20 13:10:29 +0530

    updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default async function Header() {
     const settings = await client.getSingle("settings");
 
     return (
-        <header className='top-0 z-50'>
+        <header className='top-0 z-50 mx-auto'>
 
         </header>
     )
