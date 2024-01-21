a71145f422de38ee40adfaf77a35657e6fe3ea78
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-21 18:42:09 +0530

    updated prismic.ts

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -16,10 +16,10 @@ export const repositoryName =
 // TODO: Update the routes array to match your project's route structure.
 const routes: prismic.ClientConfig["routes"] = [
   // Examples:
-  // {
-  // 	type: "homepage",
-  // 	path: "/",
-  // },
+  {
+  	type: "homepage",
+  	path: "/",
+  },
   // {
   // 	type: "page",
   // 	path: "/:uid",
