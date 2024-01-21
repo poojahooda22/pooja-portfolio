81e1cb1ac4e5f5e66daba0c7599bfca9905508ed
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-21 19:08:21 +0530

    updated

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,6 +1,8 @@
 import clsx from "clsx";
+import React from "react";
 
 type HeadingProps = {
     as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
     sixe?: "xl" | "lg" | "md" | "sm";
+    children: React.ReactNode;
 }
