d2bff33322f4511a83fe01d9c40e7e70f761ce1f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-22 20:49:24 +0530

    updated

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -79,7 +79,8 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       ref={component}
     >
-      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-1 items-center">
+      <div className="grid min-h-[70vh] 
+      grid-cols-1 md:grid-cols-1 items-center">
         <Shapes />
         <div className="col-start-1 md:row-start-1">
           <h1
