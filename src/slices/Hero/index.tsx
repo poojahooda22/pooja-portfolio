296b2ca7928faecc275840812771aca9ac5a8ef2
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-22 20:48:39 +0530

    updated classes

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -98,7 +98,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           <span 
             className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 
             to-yellow-500 bg-clip-text text-2xl 
-            font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4xl">
+            font-bold uppercase tracking-[.2em] text-transparent opacity-100 ">
             {slice.primary.tag_line}
           </span>
         </div>
