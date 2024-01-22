a433f585ed9e8f7578a0f86c8695f45db9615716
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-22 20:52:37 +0530

    updated

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -85,7 +85,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
         <Shapes />
         <div 
           className="col-start-1 md:row-start-1"
-          >
+        >
           <h1
             className="mb-8 text-[clamp(3rem,20vmin,20rem)] 
             font-extrabold leading-none tracking-tighter" 
