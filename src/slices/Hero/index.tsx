733ba2d0c7022132589c621ca15feeeec1c90c90
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-22 20:46:54 +0530

    updated index

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -89,7 +89,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             }
           >
             <span className="block text-slate-300">
-              {/* {renderLetters(slice.primary.first_name, "first")} */}
+              {renderLetters(slice.primary.first_name, "first")}
             </span>
             <span className="-mt-[.2em] block text-slate-500">
               {renderLetters(slice.primary.last_name, "last")}
