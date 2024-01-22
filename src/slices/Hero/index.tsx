76c8dc84d9baafe07f10d5fc2ed1608556d96a1d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-22 20:50:55 +0530

    updated

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -92,7 +92,8 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
               slice.primary.first_name + " " + slice.primary.last_name 
             }
           >
-            <span className="block text-slate-300">
+            <span 
+            className="block text-slate-300">
               {renderLetters(slice.primary.first_name, "first")}
             </span>
             <span className="-mt-[.2em] block text-slate-500">
