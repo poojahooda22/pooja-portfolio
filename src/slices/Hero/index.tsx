9deb70d79b40c1fe416637945416ffa6b67d80ff
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-22 20:52:51 +0530

    updated

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -81,7 +81,8 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     >
       <div 
         className="grid min-h-[70vh] 
-        grid-cols-1 md:grid-cols-1 items-center">
+        grid-cols-1 md:grid-cols-1 items-center"
+        >
         <Shapes />
         <div 
           className="col-start-1 md:row-start-1"
