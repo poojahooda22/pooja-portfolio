19c0cfc6bc429ca6a09d0588cd84f64fb632ec46
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-23 12:46:29 +0530

    updated

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -21,7 +21,7 @@ const Biography = ({ slice }: BiographyProps): JSX.Element => {
         <Heading as="h1" size="xl" className="col-start-1">
           {slice.primary.heading}
         </Heading>
-        <div className="prose prose-xl prose-slate prose-invert">
+        <div className="prose prose-xl prose-slate prose-invert col-start-1">
 
         </div>
         
