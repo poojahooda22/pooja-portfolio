16682a9ce327e36fbad99b553471d9ba3d0c6a59
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-23 11:22:25 +0530

    updated index

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -17,7 +17,7 @@ const Biography = ({ slice }: BiographyProps): JSX.Element => {
       data-slice-variation={slice.variation}
     >
       <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr, 1fr]">
-        <Heading>
+        <Heading as="h1">
           {slice.primary.heading}
         </Heading>
         
