600ab57e32342167a815a458a3fc3ec21b4a2727
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-23 13:29:10 +0530

    button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,3 +2,7 @@ import { KeyTextField, LinkField } from "@prismicio/client";
 import { PrismicNextLink } from "@prismicio/next";
 import clsx from "clsx";
 
+type ButtonProps = {
+    linkField: LinkField;
+    label: 
+}
