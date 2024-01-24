c114f3db978627a669eeda9b3a36271ccda1dfc5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-24 12:08:37 +0530

    updated avatar

--- src/slices/Biography/Avatar.tsx
+++ src/slices/Biography/Avatar.tsx
@@ -1,5 +1,6 @@
 import { ImageField } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
+import { clsx } from "clsx";
 
 import { useRef } from "react";
 
@@ -12,7 +13,7 @@ type AvatarProps = {
 export default function Avatar({ image, className}: AvatarProps) {
     const component = useRef(null);
     return (
-        <div ref={component}>
+        <div ref={component} className={clsx("relative h-full w-full", className)}>
             <div>
                 <PrismicNextImage
                     field={image}
