0ecdf78c410ee5653136c126debdd5df70dba439
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-27 19:26:56 +0530

    updated

--- src/slices/ExperienceList/index.tsx
+++ src/slices/ExperienceList/index.tsx
@@ -34,7 +34,7 @@ const ExperienceList = ({ slice }: ExperienceListProps): JSX.Element => {
           <div key={index}
             className="flex flex-col  gap-2 ml-8 font-semibold text-xl"
           >
-              {job_title}
+            {job_title}
               <div className="mb-8">
                 {job_timeline}
               </div>
