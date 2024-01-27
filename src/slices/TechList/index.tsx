ebc207a2cc337a1504d62cfb211b4334a359aa8a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-27 19:33:22 +0530

    updated

--- src/slices/TechList/index.tsx
+++ src/slices/TechList/index.tsx
@@ -77,7 +77,8 @@ const TechList = ({ slice }: TechListProps): JSX.Element => {
       {slice.items.map(({ tech_color, tech_name }, index) => (
         <div
           key={index}
-          className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
+          className="tech-row mb-8 flex items-center 
+          justify-center gap-4 text-slate-700"
           aria-label={tech_name || ""}
         >
           {Array.from({ length: 15 }, (_, index) => (
