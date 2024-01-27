9711aae9f0085fefb4d1bed0953d79579e368b44
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-27 19:24:56 +0530

    updated

--- src/slices/ExperienceList/index.tsx
+++ src/slices/ExperienceList/index.tsx
@@ -24,7 +24,8 @@ const ExperienceList = ({ slice }: ExperienceListProps): JSX.Element => {
       >
         <Heading 
           as="h2" 
-          size="lg" className="mb-8">
+          size="lg" 
+          className="mb-8">
           {slice.primary.heading}
         </Heading>
 
