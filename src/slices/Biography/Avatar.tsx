91d9b78906b5b436444ab4214d1986776620ee6e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-28 19:10:41 +0530

    updated

--- src/slices/Biography/Avatar.tsx
+++ src/slices/Biography/Avatar.tsx
@@ -53,7 +53,8 @@ export default function Avatar({ image, className}: AvatarProps) {
     }, []);
     
     return (
-        <div ref={component} className={clsx("relative h-full w-full", className)}>
+        <div 
+        ref={component} className={clsx("relative h-full w-full", className)}>
             <div className="avatar aspect-square overflow-hidden rounded-3xl border-2 border-slate-700 opacity-0f">
                 <PrismicNextImage
                     field={image}
