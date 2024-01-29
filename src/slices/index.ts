b68d65eed8de1910973f4f6c62c25e1a7950b101
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-29 20:46:12 +0530

    index file

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -4,6 +4,7 @@ import dynamic from "next/dynamic";
 
 export const components = {
   biography: dynamic(() => import("./Biography")),
+  content_index: dynamic(() => import("./ContentIndex")),
   experience_list: dynamic(() => import("./ExperienceList")),
   hero: dynamic(() => import("./Hero")),
   tech_list: dynamic(() => import("./TechList")),
