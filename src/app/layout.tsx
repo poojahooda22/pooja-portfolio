6258b6df940afb43ed41fc07c3fde5b63848f264
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-29 21:09:33 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     >
       
       <body className={inter.className}>
-        {/* <Header /> */}
+        <Header />
         {children}
       </body>
     </html>
