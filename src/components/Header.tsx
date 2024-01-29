3881b6ccf9f786c8097409d0abc9d428dbe85c3d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-29 21:21:06 +0530

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ export default async function Header() {
                         >
                             <PrismicNextLink 
                                 field={link}
-                                >
+                            >
                                 {label}
                             </PrismicNextLink>
                         </li>
