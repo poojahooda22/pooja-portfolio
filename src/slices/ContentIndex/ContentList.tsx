73d113c23abb0a1f63972add0531398efcafeb18
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-31 00:24:54 +0530

    updated

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { Content } from '@prismicio/client';
 
-function ContentList() {
+
+type ContentListProps = {
+    items: Content.BlogPostDocument[] | Content.ProjectDocument[];
+    contentType: Content.ContentIndexSlice["primary"]["content_type"];
+    fallbackItemImage: Content.ContentIndexSlice["primary"] ["fallback_item_inage"];
+    viewMoreText: Content.ContentIndexSlice["primary"]["view_more_text"];
+}
+
+function ContentList({
+    items,
+    contentType,
+    fallbackItemImage,
+    viewMoreText,
+}: ContentListProps) {
+
   return (
     <div>
-      Content List 
+      <ul>
+        {items.map((item, index) => (
+            <li key={index}>
+                <a href="">
+                    <div>
+                        <span>{item.data.title}</span>
+                        <div>
+                            {item.tags.map((tag, index) => (
+                                <span key={index}>
+                                    {tag}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+
+                </a>
+            </li>
+        ))}
+        
+      </ul>
     </div>
   )
 }
