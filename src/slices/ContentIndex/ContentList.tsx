dcadd0605e4154618c3f7dba85344ac34bd05ce5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-01-31 19:10:04 +0530

    updated

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -34,7 +34,8 @@ function ContentList({
                     <Link 
                         href={urlPrefixes + "/" + item.uid}
                         className='flex flex-col justify-between border-t 
-                        border-t-slate-100 py-10 text-slate-200 md:flex-row'
+                        border-t-slate-100 py-10 text-slate-200 
+                        md:flex-row'
                     >
                         <div className='flex flex-col'>
                             <span className='text-3xl font-bold' >{item.data.title}</span>
