8c7bfcd787847bdd8ad299e25d3b8948ca9b526e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-01 15:17:13 +0530

    updated index

--- src/slices/ExperienceList/index.tsx
+++ src/slices/ExperienceList/index.tsx
@@ -1,7 +1,7 @@
 import Bounded from "@/components/Bounded";
 import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import React from "react";
 
 /**
@@ -15,37 +15,30 @@ export type ExperienceListProps =
  */
 const ExperienceList = ({ slice }: ExperienceListProps): JSX.Element => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      <Bounded 
-        as="div"
-      >
-        <Heading 
-          as="h2" 
-          size="lg" 
-          className="mb-8"
-        >
-          {slice.primary.heading}
+    <Bounded
+    data-slice-type={slice.slice_type}
+    data-slice-variation={slice.variation}
+  >
+    <Heading as="h2" size="lg">
+      {slice.primary.heading}
+    </Heading>
+    {slice.items.map((item, index) => (
+      <div key={index} className="ml-6 mt-8 max-w-prose md:ml-12 md:mt-16">
+        <Heading as="h3" size="sm">
+          {item.job_title}
         </Heading>
 
-        {slice.items.map(({ job_title, job_timeline}, index) => (
-          <div 
-            key={index}
-            className="flex flex-col  gap-2 ml-8 
-            font-semibold text-xl"
-          >
-            {job_title}
-            <div className="mb-8">
-              {job_timeline}
-            </div>
-              
-          </div>
-          ))}
-
-      </Bounded>
-    </section>
+        <div className="mt-1 flex w-fit items-center gap-1 text-2xl font-semibold tracking-tight text-slate-400">
+          <span>{item.job_timeline}</span>{" "}
+          <span className="text-3xl font-extralight">/</span>{" "}
+          {/* <span>{item.institution}</span> */}
+        </div>
+        <div className="prose prose-lg prose-invert mt-4">
+          <PrismicRichText field={item.description} />
+        </div>
+      </div>
+    ))}
+  </Bounded>
   );
 };
 
