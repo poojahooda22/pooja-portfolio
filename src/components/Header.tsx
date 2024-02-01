cb14b5af6e840a71463ecaa1affc0fa64010eb1d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-01 14:54:54 +0530

    updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,26 +9,7 @@ export default async function Header() {
 
     return (
         <header className='top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4'>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/" aria-label="Home Page">
-                            {settings.data.name}
-                        </Link>
-                    </li>   
-                    {settings.data.nav_item.map(({link, label}, index) => (
-                        <li 
-                            key={index}
-                        >
-                            <PrismicNextLink 
-                                field={link}
-                            >
-                                {label}
-                            </PrismicNextLink>
-                        </li>
-                    ))}                
-                </ul>
-            </nav>
+            
 
         </header>
     )
