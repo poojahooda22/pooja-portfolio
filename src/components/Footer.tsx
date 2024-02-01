b4247a5de1d0d3ca66f8febd04ac03a1e4731c13
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-01 15:26:52 +0530

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,8 @@ import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
 import Bounded from "@/components/Bounded";
 import { isFilled } from "@prismicio/client";
-import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa6";
+import { FaGithub,  FaLinkedin } from "react-icons/fa6";
+import { SiLeetcode } from "react-icons/si";
 
 export default async function Footer() {
   const client = createClient();
@@ -72,7 +73,7 @@ export default async function Footer() {
               className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:scale-125 hover:text-yellow-400"
               aria-label={settings.data.name + " on Twitter"}
             >
-              <FaTwitter />
+              < SiLeetcode />
             </PrismicNextLink>
           )}
           {isFilled.link(settings.data.linkedin_link) && (
