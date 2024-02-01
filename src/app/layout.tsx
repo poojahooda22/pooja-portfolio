74f10e384f152dc65bfa8177e37da335ebab0913
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-01 15:06:14 +0530

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 const inter = Urbanist({ subsets: ['latin'] })
 
@@ -27,6 +28,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer/>
       </body>
     </html>
   )
