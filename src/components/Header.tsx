766c52c051329dd2b8d1f8f1a983bbb6be34a094
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-02 15:30:02 +0530

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ export default async function Header() {
         <header 
             className='top-0 z-50 mx-auto max-w-7xl 
             md:sticky md:top-4'
-            >
+        >
             <NavBar settings={settings} />
         </header>
     )
