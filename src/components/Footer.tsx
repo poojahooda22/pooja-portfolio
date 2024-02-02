bd6a1039956f703ea0f51ac6fa4103ae650eb4d1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-02 15:15:47 +0530

    updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,7 +23,8 @@ export default async function Footer() {
         >
           <Link
             href="/"
-            className="text-xl font-extrabold tracking-tighter text-slate-100 transition-colors duration-150 hover:text-yellow-400"
+            className="text-xl font-extrabold tracking-tighter 
+            text-slate-100 transition-colors duration-150 hover:text-yellow-400"
           >
             {settings.data.name}
           </Link>
