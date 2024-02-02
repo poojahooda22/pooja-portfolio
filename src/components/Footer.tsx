076f91da36c0fafaaee1edb1b5b4c885441cf850
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-02 15:20:14 +0530

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -74,7 +74,8 @@ export default async function Footer() {
           {isFilled.link(settings.data.github_link) && (
             <PrismicNextLink
               field={settings.data.github_link}
-              className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:scale-125 hover:text-yellow-400"
+              className="p-2 text-2xl text-slate-300 transition-all 
+              duration-150 hover:scale-125 hover:text-yellow-400"
               aria-label={settings.data.name + " on GitHub"}
             >
               <FaGithub />
