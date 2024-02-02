e1bbc9d120d607d6704388b8f563bf8072aa71d9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-02 15:07:07 +0530

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,8 @@ export default function RootLayout({
         {children}
         <Footer/>
         <div className='absolute inset-0 -z-50 max-h-screen background-gradient'></div>
-        <div className="absolute pointer-events-none inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20
+        <div className="absolute pointer-events-none inset-0 -z-40 h-full 
+        bg-[url('/noisetexture.jpg')] opacity-20
         mix-blend-soft-light "
          >
          </div>
