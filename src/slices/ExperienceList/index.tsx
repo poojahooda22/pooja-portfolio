45773e9fb62f04d8c155bea8ce9b3745c89ff3b0
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-29 18:10:10 +0530

    updated index

--- src/slices/ExperienceList/index.tsx
+++ src/slices/ExperienceList/index.tsx
@@ -17,7 +17,7 @@ const ExperienceList = ({ slice }: ExperienceListProps): JSX.Element => {
   return (
     <Bounded
       data-slice-type={slice.slice_type}
-    data-slice-variation={slice.variation}
+      data-slice-variation={slice.variation}
     >
     <Heading as="h2" size="lg">
       {slice.primary.heading}
