f2a167eb746972f995d59c613b1e37ad88d4df3e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-29 18:01:00 +0530

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         <div 
           className='absolute inset-0 -z-50 max-h-screen 
           background-gradient'
-          >
+        >
         </div>
         <div 
           className="absolute pointer-events-none 
