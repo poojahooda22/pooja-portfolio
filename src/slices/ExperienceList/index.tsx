ac2191f29ceacb345a04000e0d186ba3284c6315
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-29 18:15:26 +0530

    updated index

--- src/slices/ExperienceList/index.tsx
+++ src/slices/ExperienceList/index.tsx
@@ -28,7 +28,8 @@ const ExperienceList = ({ slice }: ExperienceListProps): JSX.Element => {
           {item.job_title}
         </Heading>
 
-        <div className="mt-1 flex w-fit items-center gap-1 text-2xl font-semibold tracking-tight text-slate-400">
+        <div className="mt-1 flex w-fit items-center 
+        gap-1 text-2xl font-semibold tracking-tight text-slate-400">
           <span>{item.job_timeline}</span>{" "}
           <span className="text-3xl font-extralight">/</span>{" "}
           <span>{item.institution}</span>
